019d36e7347565dc11d3cb2fcb6cdd904a5f0f9b
Litvinenko Maksim <ltv.maxym@gmail.com>
2026-04-09 17:09:36 +0300

    Add conform with autoformat for prettier

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,21 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			json = { "prettier" },
+			css = { "prettier" },
+			scss = { "prettier" },
+			html = { "prettier" },
+			markdown = { "prettier" },
+			yaml = { "prettier" },
+		},
+		format_after_save = {
+			timeout_ms = 1000,
+			lsp_format = "fallback",
+		},
+	},
+}
